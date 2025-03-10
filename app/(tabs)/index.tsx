a52f660028d3331da672a58acf9f092786f4d86c
Daniel Morgan <me@danielmorgan.co.uk>
2025-03-10 21:14:40 +0000

    Set up atlas and transform buffer

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,20 +1,52 @@
-import { StyleSheet, TouchableOpacity } from "react-native";
+import { useState } from "react";
+import { Image, ImageBackground, StyleSheet, TouchableOpacity } from "react-native";
+import {
+  cancelAnimation,
+  useSharedValue,
+  withRepeat,
+  withSequence,
+  withSpring,
+  withTiming,
+} from "react-native-reanimated";
 import FlockCanvas from "@/components/FlockCanvas";
 import { Text, View } from "@/components/Themed";
 
 export default function TabOneScreen() {
+  const [playing, setPlaying] = useState(false);
+  const sv = useSharedValue(0);
+
+  const handlePress = () => {
+    if (!playing) {
+      sv.value = withRepeat(withSequence(withTiming(5), withTiming(1)), -1);
+      setPlaying(true);
+    } else {
+      cancelAnimation(sv);
+      setPlaying(false);
+    }
+  };
+
   return (
     <>
-      <View style={{ pointerEvents: "none", zIndex: 10 }}>
-        <FlockCanvas />
-      </View>
+      <FlockCanvas sv={sv} />
 
-      <View style={styles.container}>
-        <Text style={styles.title}>Tab One</Text>
-        <TouchableOpacity style={styles.button}>
-          <Text style={styles.buttonText}>Press Me</Text>
+      <ImageBackground
+        source={require("@/assets/images/colored_talltrees.png")}
+        style={styles.container}
+        resizeMode="cover"
+      >
+        <View style={styles.currencyContainer}>
+          <Image
+            source={require("@/assets/images/coin_gold.png")}
+            style={{ width: 24, height: 24, aspectRatio: 1 }}
+            resizeMode="contain"
+          />
+          <Text style={styles.currency}>100</Text>
+        </View>
+        <Text style={styles.title}>Hello World</Text>
+        <TouchableOpacity style={styles.button} onPress={handlePress}>
+          <Text style={styles.buttonText}>Claim reward</Text>
         </TouchableOpacity>
-      </View>
+      </ImageBackground>
     </>
   );
 }
@@ -25,6 +57,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     gap: 10,
+    zIndex: 5,
   },
   title: {
     fontSize: 20,
@@ -36,12 +69,39 @@ const styles = StyleSheet.create({
     width: "80%",
   },
   button: {
-    backgroundColor: "forestgreen",
-    padding: 10,
-    borderRadius: 5,
+    backgroundColor: "#7629DB",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 6,
   },
   buttonText: {
-    color: "white",
+    color: "#FFFFFF",
     fontSize: 16,
+    fontWeight: "bold",
+  },
+  currencyContainer: {
+    position: "absolute",
+    top: 20,
+    right: 20,
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 10,
+    backgroundColor: "forestgreen",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 10,
+    shadowColor: "#0E2504",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 10,
+  },
+  currency: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "white",
   },
 });
--- components/FlockCanvas.tsx
+++ components/FlockCanvas.tsx
@@ -1,38 +1,55 @@
-import React from "react";
-import { Dimensions, StyleSheet } from "react-native";
-import { Canvas, useImage } from "@shopify/react-native-skia";
+import { useMemo, useState } from "react";
+import { StyleSheet, View } from "react-native";
+import {
+  Atlas,
+  Canvas,
+  Circle,
+  rect,
+  useImageAsTexture,
+  useRSXformBuffer,
+} from "@shopify/react-native-skia";
+import { SharedValue } from "react-native-reanimated";
 
-const { width, height } = Dimensions.get("window");
+const coinSize = 20;
+const NUM_SPRITES = 500;
+const sprites = new Array(NUM_SPRITES).fill(0).map(() => rect(0, 0, coinSize, coinSize));
 
-const FlockCanvas = () => {
-  const image = useImage(require("@/assets/images/pose_arms_out.png"));
+const FlockCanvas = ({ sv }: { sv: SharedValue<number> }) => {
+  const [{ width, height }, setLayout] = useState({
+    width: 0,
+    height: 0,
+  });
+  const handleOnLayout = (event: any) => {
+    const { width, height } = event.nativeEvent.layout;
+    setLayout({ width, height });
+  };
+  const c = useMemo(() => ({ x: width / 2, y: height / 2 }), [width, height]);
+  const coinTexture = useImageAsTexture(require("@/assets/images/coin_gold_sm.png"));
 
-  //
-
-  if (!image) {
-    return null;
-  }
+  const transforms2 = useRSXformBuffer(NUM_SPRITES, (val, i) => {
+    "worklet";
+    const tx = c.x + coinSize / 2 + ((i * coinSize) % width);
+    const ty = c.y - coinSize / 2 - ((i * coinSize) % height);
+    const theta = Math.atan2(c.y - ty * sv.value, c.x - tx * sv.value);
+    val.set(Math.sin(theta), Math.cos(theta), tx, ty);
+  });
 
   return (
-    <Canvas
-      style={{
-        ...StyleSheet.absoluteFillObject,
-        width,
-        height,
-        zIndex: 10,
-        // backgroundColor: "rgba(255,0,255,0.5)",
-      }}
-    >
-      {/* <Image
-        image={image}
-        x={width / 2 - 128}
-        y={100}
-        width={256}
-        height={256}
-        fit="cover"
-        opacity={0.5}
-      /> */}
-    </Canvas>
+    <View style={{ pointerEvents: "none", zIndex: 10, ...StyleSheet.absoluteFillObject }}>
+      <Canvas
+        onLayout={handleOnLayout}
+        style={{
+          zIndex: 10,
+          width: "100%",
+          height: "100%",
+          // backgroundColor: "rgba(255, 0, 255, 0.5)",
+        }}
+      >
+        <Atlas image={coinTexture} sprites={sprites} transforms={transforms2} />
+        {/* <Image image={coin} fit="cover" x={0} y={0} width={40} height={40} /> */}
+        <Circle cx={c.x} cy={c.y} r={5} color="magenta" />
+      </Canvas>
+    </View>
   );
 };
 export default FlockCanvas;
